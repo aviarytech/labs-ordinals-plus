49e2cf48cefe9a70bdd621832cf4e591aa62c465
Brian Richter <brian@aviary.tech>
2025-02-06 09:51:44 -0800

    logo spacing

--- web/src/components/Logo.tsx
+++ web/src/components/Logo.tsx
@@ -1,24 +1,21 @@
 interface LogoProps {
-  size?: 'sm' | 'md' | 'lg'
+  size?: 'md' | 'lg'
 }
 
 export default function Logo({ size = 'md' }: LogoProps) {
   const sizes = {
-    sm: 'w-5 h-5 text-[16px]',
-    md: 'w-8 h-8 text-[24px]',
-    lg: 'w-10 h-10 text-[38px]'
+    md: 'w-8 h-8 text-[32px]',
+    lg: 'w-10 h-10 text-[42px]'
   }
 
   const innerSizes = {
-    sm: 'w-4 h-4 pb-[3px]',
-    md: 'w-6 h-6 pb-[3px]',
-    lg: 'w-8 h-8 pb-[6px]'
+    md: 'w-6 h-6 pb-[4px] pr-[0.5px]',
+    lg: 'w-8 h-8 pb-[6px] pr-[0.5px]'
   }
 
   return (
     <p className={`${sizes[size]} bg-white rounded-full flex items-center justify-center border border-black`}>
-      <span className={`${innerSizes[size]} flex items-center justify-center font-bold bg-black text-white rounded-full
-                       leading-none`}>⊕</span>
+      <span className={`${innerSizes[size]} flex items-center justify-center font-bold bg-black text-white rounded-full`}>⊕</span>
     </p>
   )
 }
