cd23342e95e30215182180cb884aa937e2d9bd24
Brian Richter <brian@aviary.tech>
2025-02-19 00:12:31 -0800

    ordinals plus

--- web/src/components/Logo.tsx
+++ web/src/components/Logo.tsx
@@ -1,21 +1,24 @@
+import Image from 'next/image'
+
 interface LogoProps {
   size?: 'md' | 'lg'
 }
 
 export default function Logo({ size = 'md' }: LogoProps) {
   const sizes = {
-    md: 'w-9 h-9 text-[32px]',
-    lg: 'w-11 h-11 text-[42px]'
-  }
-
-  const innerSizes = {
-    md: 'w-7 h-7 pb-[4px] pr-[0.5px]',
-    lg: 'w-9 h-9 pb-[6px] pr-[0.5px]'
+    md: 'w-9 h-9',
+    lg: 'w-11 h-11'
   }
 
   return (
-    <p className={`${sizes[size]} bg-white rounded-full flex items-center justify-center border border-black`}>
-      <span className={`${innerSizes[size]} flex items-center justify-center font-bold bg-black text-white rounded-full`}>⊕</span>
-    </p>
+    <div className={`${sizes[size]} relative`}>
+      <Image 
+        src="/logo.png"
+        alt="Ordinals Plus Logo"
+        fill
+        className="object-contain"
+        priority
+      />
+    </div>
   )
 }
